e69668224d840fff82b226ad60b5c18d18ab0c81
Sp1r14ual <sp1r14ual@gmail.com>
2022-02-13 00:02:46 +0700

    Code Refactoring

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ from random import randint
 
 
 def randomize(n, arr_sizes):
-
+    '''
+    Generates random number within certain bounds and add this number to set
+    so it will not be generated again 
+    '''
     random_size = randint(1, UPPER_BOUND + ADD_BOUND)
     while random_size in arr_sizes:
         random_size = randint(1, UPPER_BOUND + ADD_BOUND)
@@ -14,27 +17,29 @@ def randomize(n, arr_sizes):
 
 
 def sort_arr(arr, index):
-
+    '''
+    Returns normally sorted array if index is even
+    and sorted reversely if index is odd
+    '''
     return sorted(arr) if index % 2 == 0 else sorted(arr, reverse=True)
 
 
 def solve(n):
+    '''Main function for algorithm'''
 
     arrays = list()
     arr_sizes = set()
-
+    #Set is for unique array sizes
 
     for i in range(1, n + 1):
 
         new_arr = list()
 
         random_size = randomize(n, arr_sizes)
-        
         for _ in range(random_size):
             new_arr.append(randint(LOWER_BOUND, UPPER_BOUND))
 
         new_arr = sort_arr(new_arr, i)
-
         arrays.append(new_arr)
 
     return arrays
@@ -49,7 +54,3 @@ ADD_BOUND = 10
 solution = solve(n)
 for i in range(len(solution)):
     print(solution[i])
-
-
-# nsk.school.handh.ru
-# docs.google.com/document/d/1lfpe1JDCuGMQ3cFyn5oNk2PqRO94z6IqCq6yoTaUsYo
